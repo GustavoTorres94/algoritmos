d4805cfcc99523f3ef54f8091b3c44200da876d8
Gustavo <gustavo.t.paula@gmail.com>
2024-01-29 23:57:55 -0300

    2 desafios

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+
+    if target_time is None or not isinstance(target_time, int):
+        return None
+
+    for tupla in permanence_period:
+        if not all(isinstance(i, int) for i in tupla):
+            return None
+
+        if target_time in tupla:
+            count += 1
+    print(count)
+    return count
+
+
+study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)], 3)
