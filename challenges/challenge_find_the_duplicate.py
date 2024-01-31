8e62a6407d7a67929e4d1046c442181f60c87628
Gustavo <gustavo.t.paula@gmail.com>
2024-01-31 19:14:29 -0300

    melhoria da logica

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,14 +1,13 @@
 def find_duplicate(nums):
     if not isinstance(nums, list) or len(nums) <= 1:
         return False
-    set_numbers = set()
-    for num in nums:
-        if not isinstance(num, int) or num < 0:
+    sorted_num = sorted(nums)
+    for i in range(len(sorted_num) - 1):
+        if not isinstance(sorted_num[i], int) or sorted_num[i] < 0:
             return False
-        if num in set_numbers:
-            return True
-        set_numbers.add(num)
+        if sorted_num[i] == sorted_num[i + 1]:
+            return sorted_num[i]
     return False
 
 
-find_duplicate([1, 3, 4, 2, 2])
+find_duplicate(["a", "b"])
