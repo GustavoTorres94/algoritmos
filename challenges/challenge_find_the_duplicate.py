aff19da7c222e9622ebbdd4ef224e2ac247dfa3f
Gustavo <gustavo.t.paula@gmail.com>
2024-01-31 18:47:25 -0300

    baraban?

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    sorted_nums = sorted(nums)
+    if not isinstance(nums, list) or len(nums) == 1:
+        return False
+    for i in range(len(sorted_nums)):
+        if not isinstance(sorted_nums[i], int):
+            return False
+        if sorted_nums[i] < 0:
+            return False
+        if sorted_nums[i] == sorted_nums[i + 1]:
+            return print(sorted_nums[i])
+    return False
+
+
+find_duplicate([1, 3, 4, 2, 2])
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,14 +2,9 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    invalid_key = "a"
-    raise_error = TypeError
-    assert encrypt_message("abc", invalid_key) == raise_error(
-        TypeError, "tipo inválido para key"
-    )
-    invalid_message = 112233
-    assert encrypt_message(invalid_message, 2) == raise_error(
-        TypeError, "tipo inválido para message"
+    assert encrypt_message("abc", "a") == TypeError("tipo inválido para key")
+    assert encrypt_message(112233, 2) == TypeError(
+        "tipo inválido para message"
     )
     assert encrypt_message("abc", 2) == "a_cba"
     assert encrypt_message("abc", 3) == "cba_"
