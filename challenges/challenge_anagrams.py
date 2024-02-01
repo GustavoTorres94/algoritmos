02e005af7ffaeffcf8c9b1233673fdeb3a5dc666
Gustavo <gustavo.t.paula@gmail.com>
2024-02-01 14:51:27 -0300

    4 quase

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,22 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aquiaa."""
-    raise NotImplementedError
+    if not isinstance(first_string, str) or not isinstance(second_string, str):
+        raise TypeError("As entradas devem ser strings")
+
+    if not first_string or not second_string:
+        return False
+
+    str1_sorted = "".join(bubble_sort(list(first_string.lower())))
+    str2_sorted = "".join(bubble_sort(list(second_string.lower())))
+
+    return str1_sorted, str2_sorted, str1_sorted == str2_sorted
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,13 +1,25 @@
 from challenges.challenge_encrypt_message import encrypt_message
 
+import pytest
+
 
 def test_encrypt_message():
-    assert encrypt_message("abc", "a") == TypeError("tipo inválido para key")
-    assert encrypt_message(112233, 2) == TypeError(
-        "tipo inválido para message"
-    )
     assert encrypt_message("abc", 2) == "a_cba"
     assert encrypt_message("abc", 3) == "cba_"
     assert encrypt_message("abc", 0) == "cba_"
     assert encrypt_message("abc", -1) == "cba_"
-    assert encrypt_message("abc", 2) != "bca_"
+    with pytest.raises(TypeError) as error:
+        encrypt_message("abc", "a")
+        assert str(error.value) == "tipo inválido para key"
+    with pytest.raises(TypeError) as error:
+        encrypt_message(112233, 2)
+        assert str(error.value) == "tipo inválido para message"
+    # assert encrypt_message("abc", "a") == TypeError("tipo inválido para key")
+    # assert encrypt_message(112233, 2) == TypeError(
+    #     "tipo inválido para message"
+    # )
+    # assert encrypt_message("abc", 2) == "a_cba"
+    # assert encrypt_message("abc", 3) == "cba_"
+    # assert encrypt_message("abc", 0) == "cba_"
+    # assert encrypt_message("abc", -1) == "cba_"
+    # assert encrypt_message("abc", 2) != "bca_"
