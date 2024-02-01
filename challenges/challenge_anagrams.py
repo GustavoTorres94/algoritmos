90c12007b8e2ab4cd73e93fba1d1202c7d118376
Gustavo <gustavo.t.paula@gmail.com>
2024-02-01 18:17:15 -0300

    teste2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,22 +1,48 @@
-def bubble_sort(arr):
-    n = len(arr)
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
 
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-    return arr
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
 
 
 def is_anagram(first_string, second_string):
-    if not isinstance(first_string, str) or not isinstance(second_string, str):
-        raise TypeError("As entradas devem ser strings")
-
-    if len(first_string) == 0 or len(second_string) == 0:
+    if not first_string or not second_string:
         return False
 
-    str1_sorted = "".join(bubble_sort(list(first_string.lower())))
-    str2_sorted = "".join(bubble_sort(list(second_string.lower())))
+    arr1 = list(first_string.lower())
+    arr2 = list(second_string.lower())
+
+    merge_sort(arr1)
+    merge_sort(arr2)
+
+    str1_sorted = "".join(arr1)
+    str2_sorted = "".join(arr2)
 
     return str1_sorted, str2_sorted, str1_sorted == str2_sorted
+
+
+is_anagram("amor", "roma")
